f63596a642497f2993b85984a33784b3e0eb1495
Ayrahhh <abdulrahimalwiya91@gmail.com>
2023-01-04 17:16:30 +0300

    first commit

---
+++ src/main/java/com/example/municipalSpringboot/MunicipalSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.municipalSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MunicipalSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MunicipalSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/municipalSpringboot/MunicipalSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.municipalSpringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MunicipalSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
